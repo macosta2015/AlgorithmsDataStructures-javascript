55c5c2cf07b9ad0910f5dc6175debd579278bc6a
macosta2015 <macosta2015@my.fit.edu>
2023-03-14 17:47:35 -0400

    Update SearchAlgorithms.js

--- JavaScriptTests/SearchAlgorithms.js
+++ JavaScriptTests/SearchAlgorithms.js
@@ -1,5 +1,13 @@
+// Big O time complexity, as the number of the size increases, so does the search.
 function linearSearch(arr, target) {
-
-
-    return
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === target) {
+            return i
+        }
+    }
+    return ("None")
 }
+
+console.log(linearSearch([-5, 2, 10, 4, 6], 10))
+console.log(linearSearch([-5, 2, 10, 4, 6], 6))
+console.log(linearSearch([-5, 2, 10, 4, 6], 20))
