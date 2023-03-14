f86cb516dee6db1d85e61bae4fba0f9295ba2a7e
Acosta Mario <macosta@titanamerica.com>
2023-03-14 08:12:25 -0400

    Update Recursion.js

--- JavaScriptTests/Recursion.js
+++ JavaScriptTests/Recursion.js
@@ -1,8 +1,10 @@
+//The Fibonacci position's of F(0),F(1), and F(2) are equal to 1
 function recursiveFibonacci(n) {
     if (n < 2) {
         return n
     }
+    console.log('n before recursion: ' + n)
     return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2)
 }
 
-console.log(recursiveFibonacci(3))
+console.log(recursiveFibonacci(4))
