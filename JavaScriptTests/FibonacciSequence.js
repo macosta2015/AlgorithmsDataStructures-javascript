8681a7d9056e4b8703a3fb65b5ca6f135ec6f3cc
Acosta Mario <macosta@titanamerica.com>
2023-03-14 08:25:46 -0400

    Update FibonacciSequence.js

--- JavaScriptTests/FibonacciSequence.js
+++ JavaScriptTests/FibonacciSequence.js
@@ -1,7 +1,9 @@
+//The Fibonacci position's of F(0),F(1), and F(2) are equal to 1
+
 function fibonacci(n) {
     const fib = [0, 1]
     for (let i = 2; i < n; i++) {
-        fib[i] = fib[i - 1] - fib[i - 2]
+        fib[i] = fib[i - 1] + fib[i - 2]
     }
     return fib
 }
