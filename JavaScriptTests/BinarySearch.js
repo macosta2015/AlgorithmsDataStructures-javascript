87054b799f3151254755f56b1922f0e11d127a8b
macosta2015 <macosta2015@my.fit.edu>
2023-03-14 18:20:30 -0400

    Update BinarySearch.js

--- JavaScriptTests/BinarySearch.js
+++ JavaScriptTests/BinarySearch.js
@@ -1,1 +1,25 @@
-//The array needs to be in order
+//The array needs to be in order.
+// Big-O = O(logn)
+
+function binarySearch(arr, target) {
+    let leftIndex = 0
+    let rightIndex = arr.length - 1
+
+    while (leftIndex <= rightIndex) {
+        let middleIndex = Math.floor((leftIndex + rightIndex) / 2)
+        if (target === arr[middleIndex]) {
+            return middleIndex
+        }
+        if (target < arr[middleIndex]) {
+            rightIndex = middleIndex - 1;
+        } else {
+            leftIndex = middleIndex + 1;
+        }
+    }
+    return "None"
+}
+
+console.log(binarySearch([-5, 2, 4, 6, 10], 4))
+console.log(binarySearch([-5, 2, 4, 6, 10], 6))
+console.log(binarySearch([-5, 2, 4, 6, 10], 20))
+
