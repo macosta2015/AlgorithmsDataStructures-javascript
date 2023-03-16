9188e322b9b379e7480deaf17ec0da706edf08f0
Acosta Mario <macosta@titanamerica.com>
2023-03-16 18:47:38 -0400

    Update QuickSort.js

--- JavaScriptTests/QuickSort.js
+++ JavaScriptTests/QuickSort.js
@@ -1,3 +1,23 @@
 //Worst case - O(n^2)
 //Average case -O(nlogn)
 //It is really unlikely that you will sort and already sorted algorithm.
+
+function quickSort(arr) {
+    if (arr.length < 2) {
+        return arr
+    }
+    let pivot = arr[arr.length - 1]
+    let left = []
+    let right = []
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] < pivot) {
+            left.push(arr[i])
+        } else {
+            right.push(arr[i])
+        }
+    }
+    return [...quickSort(left), pivot, ...quickSort(right)]
+}
+
+const arr = [8, 20, -2, 4, -6]
+console.log(quickSort(arr))
