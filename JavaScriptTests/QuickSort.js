3d5e9d9bb8a57269c01e1b2f7ddffb2258afebf5
macosta2015 <macosta2015@my.fit.edu>
2023-03-15 23:27:14 -0400

    Update QuickSort.js

--- JavaScriptTests/QuickSort.js
+++ JavaScriptTests/QuickSort.js
@@ -1,3 +1,3 @@
 //Worst case - O(n^2)
 //Average case -O(nlogn)
-//It is really unlikely that you will sort and already sorted algorithm 
+//It is really unlikely that you will sort and already sorted algorithm.
