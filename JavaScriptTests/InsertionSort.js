26a0c1f8ed9f0102cc6d3d223b428c060bc856f4
macosta2015 <macosta2015@my.fit.edu>
2023-03-15 22:48:02 -0400

    Update InsertionSort.js

--- JavaScriptTests/InsertionSort.js
+++ JavaScriptTests/InsertionSort.js
@@ -1,7 +1,10 @@
 //We assume that the first element is sorted
 
 function insertionSort(arr) {
-
+    for (let i = 1; i < arr.length; i++) {
+        let numberToInsert = arr[i]
+        let j = i - 1
+    }
 }
 
 const arr = [8, 20, -2, 4, -6]
