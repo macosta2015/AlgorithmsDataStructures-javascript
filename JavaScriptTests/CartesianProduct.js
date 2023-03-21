e064f383d931b9e547f13d1129f9ced806c630ba
macosta2015 <macosta2015@my.fit.edu>
2023-03-21 17:09:40 -0400

    Update CartesianProduct.js

--- JavaScriptTests/CartesianProduct.js
+++ JavaScriptTests/CartesianProduct.js
@@ -3,6 +3,7 @@ function cartesianProduct(arr1, arr2) {
     for (let i = 0; i < arr1.length; i++) {
         for (let j = 0; j < arr2.length; j++) {
             result.push([arr1[i], arr2[j]])
+            console.log('This is the result' + result)
         }
     }
     return result
